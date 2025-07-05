4b548630773ca81f8858fd6d6de4b5d782720f23
Ruan Barros <ruan.personal@peppo.dev>
2025-07-05 16:17:39 -0300

    fix: remove unecessary dependencies

--- server/src/controllers/report.controller.ts
+++ server/src/controllers/report.controller.ts
@@ -1,5 +1,3 @@
-import { BadRequestError } from '@/errors/bad-request-error'
-import { isRight } from '@/shared/either'
 import { exportUploads } from '@/use-cases/export-uploads'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
@@ -19,9 +17,7 @@ export const reportController: FastifyPluginAsyncZod = async server => {
 		},
 		async (_request, reply) => {
 			const result = await exportUploads()
-			if (isRight(result))
-				return reply.code(200).send({ url: result.right.reportUrl })
-			throw new BadRequestError('Failed to generate report')
+			return reply.code(200).send({ url: result.reportUrl })
 		}
 	)
 }
--- server/src/use-cases/export-uploads.ts
+++ server/src/use-cases/export-uploads.ts
@@ -1,69 +1,64 @@
-import { db, pg } from '@/infra/db'
-import { schema } from '@/infra/db/schemas'
 import { PassThrough, Transform } from 'node:stream'
 import { pipeline } from 'node:stream/promises'
-import { type Either, makeRight } from '@/shared/either'
-import { stringify } from 'csv-stringify'
+import { db, pg } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
 import { uploadFileToStorage } from '@/infra/storage/upload'
+import { stringify } from 'csv-stringify'
 
 type ExportUploadsOutput = {
-    reportUrl: string
+	reportUrl: string
 }
 
-export async function exportUploads(): Promise<Either<never, ExportUploadsOutput>> {
-    const { sql, params: sqlParams } = db
-        .select()
-        .from(schema.links)
-        .toSQL()
+export async function exportUploads(): Promise<ExportUploadsOutput> {
+	const { sql, params: sqlParams } = db.select().from(schema.links).toSQL()
 
-    const cursor = pg.unsafe(sql, sqlParams as string[]).cursor(1)
+	const cursor = pg.unsafe(sql, sqlParams as string[]).cursor(1)
 
-    const csv = stringify({
-        delimiter: ',',
-        header: true,
-        columns: [
-            { key: 'id', header: 'ID' },
-            { key: 'originalUrl', header: 'Original Url' },
-            { key: 'shortUrl', header: 'Short Url' },
-            { key: 'created_at', header: 'Created at' },
-        ],
-    })
+	const csv = stringify({
+		delimiter: ',',
+		header: true,
+		columns: [
+			{ key: 'id', header: 'ID' },
+			{ key: 'originalUrl', header: 'Original Url' },
+			{ key: 'shortUrl', header: 'Short Url' },
+			{ key: 'created_at', header: 'Created at' },
+		],
+	})
 
-    const pipelinePassThroughStream = new PassThrough()
+	const pipelinePassThroughStream = new PassThrough()
 
-    const uploadCsv = uploadFileToStorage({
-        contentType: 'text/csv',
-        fileName: `${new Date().toISOString()}-links.csv`,
-        folder: 'downloads',
-        contentStream: pipelinePassThroughStream,
-    })
+	const uploadCsv = uploadFileToStorage({
+		contentType: 'text/csv',
+		fileName: `${new Date().toISOString()}-links.csv`,
+		folder: 'downloads',
+		contentStream: pipelinePassThroughStream,
+	})
 
-    const pipelineStream = pipeline(
-        cursor,
-        new Transform({
-            objectMode: true,
-            transform(chunks: unknown[], _encoding, callback) {
-                for (const chunk of chunks) {
-                    this.push(normalizeData(chunk))
-                }
-                callback()
-            },
-        }),
-        csv,
-        pipelinePassThroughStream
-    )
+	const pipelineStream = pipeline(
+		cursor,
+		new Transform({
+			objectMode: true,
+			transform(chunks: unknown[], _encoding, callback) {
+				for (const chunk of chunks) {
+					this.push(normalizeData(chunk))
+				}
+				callback()
+			},
+		}),
+		csv,
+		pipelinePassThroughStream
+	)
 
-    const [{ url }] = await Promise.all([uploadCsv, pipelineStream])
+	const [{ url }] = await Promise.all([uploadCsv, pipelineStream])
 
-    return makeRight({ reportUrl: url })
+	return { reportUrl: url }
 }
-
 
 export function normalizeData(chunk: any) {
-    return ({
-        id: chunk.id,
-        originalUrl: chunk.original_url,
-        shortUrl: `https://brevly.com/${chunk.short_url}`,
-        created_at: chunk.created_at,
-    })
+	return {
+		id: chunk.id,
+		originalUrl: chunk.original_url,
+		shortUrl: `https://brevly.com/${chunk.short_url}`,
+		created_at: chunk.created_at,
+	}
 }
